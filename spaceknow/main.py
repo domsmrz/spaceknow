6e7820ecbcbf6f181c827ddd792171e8dda110af
Dominik Smrž <dom.smrz@gmail.com>
2021-03-23 20:56:10 +0100

    Basic implementation of image and detection retrieval

--- spaceknow/main.py
+++ spaceknow/main.py
@@ -1,12 +1,63 @@
 import argparse
 import json
-from spaceknow import spaceknow_api
+import io
+import PIL.Image
+import PIL.ImageDraw
 from typing import Dict
+import requests
+import math
+
+from spaceknow import spaceknow_api
 
 
 DATASETS = [
     ('gbdx', ['idaho-pansharpened']),
 ]
+
+
+def draw_geometry(image: PIL.Image, draw: PIL.ImageDraw, geometry: Dict, bands):
+    if geometry['type'] != 'Polygon':
+        raise NotImplementedError("Drawing non-polygon geometries is not implemented yet")
+
+    selected_band = bands[0]  # FIXME: I not sure if taking the first band is the right way to go
+
+    image_coordinates = [
+        (
+            math.floor((xc - selected_band['crsOriginX']) / selected_band['pixelSizeX']),
+            math.floor((yc - selected_band['crsOriginY']) / selected_band['pixelSizeY']),
+        )
+        for xc, yc in geometry['coordinates'][0][:-1]
+    ]
+    draw.polygon(image_coordinates, fill="#ff0000")
+
+
+def create_image(api: spaceknow_api.SpaceKnowApi, imagery_data, car_data, bands):
+    if set(map(tuple, imagery_data['tiles'])) != set(map(tuple, car_data['tiles'])):
+        raise RuntimeError("Non-matching set of tiles for imagery and car analysis")
+
+    tiles = imagery_data['tiles']
+    if len({z for z, _, _ in tiles}) != 1:
+        raise RuntimeError("Distinct zoom levels in one scene")
+
+    min_x = min(x for _, x, _ in tiles)
+    min_y = min(y for _, _, y in tiles)
+    max_x = min(x for _, x, _ in tiles)
+    max_y = min(y for _, _, y in tiles)
+
+    images = list()
+    for z, x, y in tiles:
+        image = PIL.Image.open(io.BytesIO(api.get(f'/kraken/grid/{imagery_data["mapId"]}/-/{z}/{x}/{y}/truecolor.png')))
+        draw = PIL.ImageDraw.Draw(image)
+
+        tile_car_data = json.loads(api.get(f'/kraken/grid/{car_data["mapId"]}/-/{z}/{x}/{y}/detections.geojson'))
+        if tile_car_data['type'] != 'FeatureCollection':
+            raise RuntimeError(f"Unknown data type {tile_car_data['type']!r}")
+
+        for single_car in tile_car_data['features']:
+            draw_geometry(image, draw, single_car['geometry'], bands)
+
+        images.append((image, x - min_x, y - min_y))
+        image.show()
 
 
 def process_dataset(api: spaceknow_api.SpaceKnowApi, geometry: Dict, provider: str, dataset: str) -> None:
@@ -24,12 +75,18 @@ def process_dataset(api: spaceknow_api.SpaceKnowApi, geometry: Dict, provider: s
     data = api.retrieve(api.initiate('/imagery/search', data=input_data))
     if data['cursor'] is not None:
         raise NotImplementedError("Cannot deal with paging yet")
-    scene_ids = [x['sceneId'] for x in data['results']][:1]  # TODO: to make it faster
+    data_results = data['results'][:1]
+    scene_ids = [x['sceneId'] for x in data_results]
+    bands = [x['bands'] for x in data_results]
     imagery_pids, car_pids = (
         [api.initiate(f'/kraken/release/{map_type}/geojson', data={'sceneId': sid, 'extent': extent})
          for sid in scene_ids]
         for map_type in ('imagery', 'cars')
     )
+    for i, (imagery_pid, car_pid, item_bands) in enumerate(zip(imagery_pids, car_pids, bands)):
+        imagery_data = api.retrieve(imagery_pid)
+        car_data = api.retrieve(car_pid)
+        create_image(api, imagery_data, car_data, item_bands)
 
 
 def main() -> None:
